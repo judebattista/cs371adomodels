2971bae8475116b4d831d7de1f6d827781ac6ffc
Jude Battista <jbattista20@my.whitworth.edu>
2020-01-14 05:57:02 -0800

    Add basis for using models with ADO

---
+++ cs371entityframework/MySqlDb.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+using cs371entityframework.Models;
+
+namespace cs371entityframework
+{
+    public class MySqlDb
+    {
+        private MySqlConnection conn;
+
+        public MySqlDb(string server, string user, string pw, string db) {
+            var connStringBuilder = new MySqlConnectionStringBuilder
+            {
+                Server = server,
+                UserID = user,
+                Password = pw,
+                Database = db,
+                OldGuids = true
+            };
+
+            string connstr = connStringBuilder.ConnectionString;
+            conn = new MySqlConnection(connstr);
+        }
+
+        public void OpenConnection() {
+            conn.Open();
+        }
+
+        public void CloseConnection() {
+            conn.Close();
+        }
+
+        public List<Ship> GetAllShips() {
+            List<Ship> ships = new List<Ship>();
+            string sql = "SELECT * FROM ships";
+            using (MySqlCommand cmd = new MySqlCommand()) {
+                cmd.CommandText = sql;
+                cmd.Connection = conn;
+                MySqlDataReader reader = cmd.ExecuteReader(System.Data.CommandBehavior.CloseConnection);
+                while (reader.Read()) {
+                    ships.Add(new Ship { 
+                        Id = (int)reader["id"],
+                        Name = (string)reader["Name"],
+                        Registration = (string)reader["registration"]
+                    });
+                }
+                reader.Close();
+            }
+            return ships;
+        }
+
+        // In class - Add method to get a single ship
+
+        // Homework four: models and method to print out a complete roster
+        //      Crew member's full name, age and the role they fill, the ship's name and registration number
+
+    }  
+}
+            
+            
+            
+
---
+++ cs371entityframework/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+using cs371entityframework.Models;
+
+namespace cs371entityframework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MySqlDb fleet = new MySqlDb("db4free.net", "cs371student", "whitworth", "cs371ado");
+            fleet.OpenConnection();
+            List<Ship> ships = fleet.GetAllShips();
+            foreach (Ship s in ships) {
+                Console.WriteLine("{0} has registration number {1}", s.Name, s.Registration);
+            }
+
+        }
+    }
+}
